84b816f0f596a148c1c3592599051d8ca89acbce
Daniel Muñoz Heredia <dsgpolaris55@gmail.com>
2023-11-30 10:27:14 +0100

    Delete vulnet_app directory

--- vulnet_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class VulnetAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vulnet_app'
-
-    def ready(self):
-        import vulnet_app.signals
